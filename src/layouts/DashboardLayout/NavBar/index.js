1ca02c63033bb9d992be83d23c8a06b92e0c4a2b
Fusereact <jackcrmdatabase@gmail.com>
2021-02-12 15:23:29 +0500

    first commit

---
+++ src/layouts/DashboardLayout/NavBar/index.js
@@ -0,0 +1,200 @@
+import React, { useEffect } from 'react';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  Hidden,
+  List,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import {
+  AlertCircle as AlertCircleIcon,
+  BarChart as BarChartIcon,
+  Lock as LockIcon,
+  Settings as SettingsIcon,
+  ShoppingBag as ShoppingBagIcon,
+  User as UserIcon,
+  UserPlus as UserPlusIcon,
+  Users as UsersIcon,
+  Home as HomeIcon,
+  Disc as DiscIcon,
+  Music as MusicIcon,
+  Info as InfoIcon,
+  Anchor as AnchorIcon,
+  Clipboard as ClipboardIcon
+} from 'react-feather';
+import NavItem from './NavItem';
+
+const user = {
+  avatar:
+    'https://images.pexels.com/photos/2853535/pexels-photo-2853535.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+  jobTitle: 'Admin',
+  name: 'Admin Name'
+};
+
+const items = [
+  {
+    href: '/app/dashboard',
+    icon: BarChartIcon,
+    title: 'Dashboard'
+  },
+  {
+    href: '/app/managehomepage',
+    icon: HomeIcon,
+    title: 'Manage Homepage'
+  },
+  {
+    href: '/app/customers',
+    icon: UsersIcon,
+    title: 'Users'
+  },
+  {
+    href: '/app/artists',
+    icon: UserIcon,
+    title: 'Artists'
+  },
+  {
+    href: '/app/products',
+    icon: DiscIcon,
+    title: 'Albums'
+  },
+  {
+    href: '/app/products',
+    icon: MusicIcon,
+    title: 'Songs'
+  },
+  {
+    href: '/app/account',
+    icon: InfoIcon,
+    title: 'Basic Info'
+  },
+  {
+    href: '/app/settings',
+    icon: ClipboardIcon,
+    title: 'Categories'
+  },
+  {
+    href: '/app/settings',
+    icon: AnchorIcon,
+    title: 'Genres'
+  }
+  // {
+  //   href: '/login',
+  //   icon: LockIcon,
+  //   title: 'Login'
+  // },
+  // {
+  //   href: '/register',
+  //   icon: UserPlusIcon,
+  //   title: 'Register'
+  // },
+  // {
+  //   href: '/404',
+  //   icon: AlertCircleIcon,
+  //   title: 'Error'
+  // }
+];
+
+const useStyles = makeStyles(() => ({
+  mobileDrawer: {
+    width: 256
+  },
+  desktopDrawer: {
+    width: 256,
+    top: 64,
+    height: 'calc(100% - 64px)'
+  },
+  avatar: {
+    cursor: 'pointer',
+    width: 64,
+    height: 64
+  }
+}));
+
+const NavBar = ({ onMobileClose, openMobile }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (openMobile && onMobileClose) {
+      onMobileClose();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
+
+  const content = (
+    <Box height="100%" display="flex" flexDirection="column">
+      <Box alignItems="center" display="flex" flexDirection="column" p={2}>
+        <Avatar
+          className={classes.avatar}
+          component={RouterLink}
+          src={user.avatar}
+          to="/app/account"
+        />
+        <Typography className={classes.name} color="textPrimary" variant="h5">
+          {user.name}
+        </Typography>
+        <Typography color="textSecondary" variant="body2">
+          {user.jobTitle}
+        </Typography>
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <List>
+          {items.map(item => (
+            <NavItem
+              href={item.href}
+              key={item.title}
+              title={item.title}
+              icon={item.icon}
+            />
+          ))}
+        </List>
+      </Box>
+      <Box flexGrow={1} />
+    </Box>
+  );
+
+  return (
+    <>
+      <Hidden lgUp>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.mobileDrawer }}
+          onClose={onMobileClose}
+          open={openMobile}
+          variant="temporary"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+      <Hidden mdDown>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.desktopDrawer }}
+          open
+          variant="persistent"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+    </>
+  );
+};
+
+NavBar.propTypes = {
+  onMobileClose: PropTypes.func,
+  openMobile: PropTypes.bool
+};
+
+NavBar.defaultProps = {
+  onMobileClose: () => {},
+  openMobile: false
+};
+
+export default NavBar;
---
+++ src/views/customer/CustomerListView/Toolbar.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  makeStyles
+} from '@material-ui/core';
+import { Search as SearchIcon } from 'react-feather';
+import { UserPlus as UserPlusIcon, Filter as FilterIcon } from 'react-feather';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  importButton: {
+    marginRight: theme.spacing(1)
+  },
+  exportButton: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const Toolbar = ({ className, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(classes.root, className)} {...rest}>
+      <Box display="flex" justifyContent="flex-end">
+        {/* <Button className={classes.importButton}>Import</Button>
+        <Button className={classes.exportButton}>Export</Button> */}
+        <Button className="mr-2" color="primary" variant="contained">
+          <SvgIcon fontSize="small" color="white">
+            <FilterIcon />
+          </SvgIcon>
+          Filters
+        </Button>
+        <Button color="primary" variant="contained">
+          <SvgIcon fontSize="small" color="white">
+            <UserPlusIcon />
+          </SvgIcon>
+          {'  '}
+          Add User
+        </Button>
+      </Box>
+      <Box mt={3}>
+        <Card>
+          <CardContent>
+            <Box maxWidth={500}>
+              <TextField
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SvgIcon fontSize="small" color="action">
+                        <SearchIcon />
+                      </SvgIcon>
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="Search User"
+                variant="outlined"
+              />
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </div>
+  );
+};
+
+Toolbar.propTypes = {
+  className: PropTypes.string
+};
+
+export default Toolbar;
